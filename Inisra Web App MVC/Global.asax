15a3b4023e9ff1387d6e029f388db021a72ab12d
Ferido07 <feridyz@gmail.com>
2017-11-26 17:07:06 +0300

    Add project files.

---
+++ Inisra Web App MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Inisra_Web_App_MVC.MvcApplication" Language="C#" %>
---
+++ Inisra Web App MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Inisra_Web_App_MVC.Startup))]
+namespace Inisra_Web_App_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
