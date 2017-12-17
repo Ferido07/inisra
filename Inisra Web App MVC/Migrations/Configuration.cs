e4406f51064a513f715af12b9a20ff4ac8b3b997
Ferido07 <feridyz@gmail.com>
2017-12-17 12:27:26 +0300

    Enabled Migration for IdentityContext and deleted all the tales already existing in InisraDB.

---
+++ Inisra Web App MVC/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace Inisra_Web_App_MVC.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Inisra_Web_App_MVC.DAL.InisraContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Inisra_Web_App_MVC.DAL.InisraContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
