8090c6a917a6b382c712326a8b9ad5692a6ace21
Ferido07 <feridyz@gmail.com>
2018-04-05 12:02:04 +0300

    Changed ProfileController to use BLL intstead of DAL. Except for UploadCV action.

--- Inisra Web App MVC/Controllers/ProfileController.cs
+++ Inisra Web App MVC/Controllers/ProfileController.cs
@@ -1,11 +1,8 @@
-﻿using Inisra_Web_App_MVC.DAL;
+﻿using Inisra_Web_App_MVC.BLL;
 using Inisra_Web_App_MVC.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
-using System;
-using System.Data.Entity;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -15,7 +12,7 @@ namespace Inisra_Web_App_MVC.Controllers
     [Authorize(Roles ="JobSeeker")]
     public class ProfileController : Controller
     {
-        private InisraContext db = new InisraContext();
+        private JobSeekerBLL bll = new JobSeekerBLL();
         private InisraUserManager _userManager;
 
         public InisraUserManager UserManager
@@ -40,7 +37,7 @@ namespace Inisra_Web_App_MVC.Controllers
         public async Task<ActionResult> Details()
         {
             var jobSeekerUser = (JobSeekerUser)(await UserManager.FindByIdAsync(User.Identity.GetUserId()));
-            JobSeeker jobSeeker = await db.JobSeekers.FindAsync(jobSeekerUser.JobSeekerID);
+            JobSeeker jobSeeker = await bll.GetJobSeekerById(jobSeekerUser.JobSeekerID.Value);
             //just in case but almost never happens
             if (jobSeeker == null)
             {
@@ -55,14 +52,14 @@ namespace Inisra_Web_App_MVC.Controllers
         public async Task<ActionResult> Edit()
         {
             var jobSeekerUser = (JobSeekerUser)(await UserManager.FindByIdAsync(User.Identity.GetUserId()));
-            JobSeeker jobSeeker = await db.JobSeekers.FindAsync(jobSeekerUser.JobSeekerID);
+            JobSeeker jobSeeker = await bll.GetJobSeekerById(jobSeekerUser.JobSeekerID.Value);
             //just in case but almost never happens
             if (jobSeeker == null)
             {
                 return HttpNotFound();
             }
             //todo remove and replace the dropdown for location with textbox
-            ViewBag.LocationID = new SelectList(db.Locations, "ID", "Name", jobSeeker.LocationID);
+            //ViewBag.LocationID = new SelectList(db.Locations, "ID", "Name", jobSeeker.LocationID);
             return View(jobSeeker);
         }
 
@@ -71,22 +68,20 @@ namespace Inisra_Web_App_MVC.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "ID,FirstName,LastName,Email,PhoneNo,IsFemale,Birthday,LocationID")] JobSeeker jobSeeker)
+        public async Task<ActionResult> Edit([Bind(Include = "ID,FirstName,LastName,Email,PhoneNo,IsFemale,Birthday")] JobSeeker jobSeeker)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(jobSeeker).State = EntityState.Modified;
-                await db.SaveChangesAsync();
+                await bll.UpdateJobSeeker(jobSeeker);
                 return RedirectToAction("Index");
             }
-            ViewBag.LocationID = new SelectList(db.Locations, "ID", "Name", jobSeeker.LocationID);
             return View(jobSeeker);
         }
 
         public async Task<ActionResult> Resume()
         {
             var jobSeekerUser = (JobSeekerUser)(await UserManager.FindByIdAsync(User.Identity.GetUserId()));
-            JobSeeker jobSeeker = await db.JobSeekers.FindAsync(jobSeekerUser.JobSeekerID);
+            JobSeeker jobSeeker = await bll.GetJobSeekerById(jobSeekerUser.JobSeekerID.Value);
             ViewBag.ResumeCount = jobSeeker.CVs.Count;
             return View();
         }
@@ -97,7 +92,7 @@ namespace Inisra_Web_App_MVC.Controllers
             if (file != null && file.ContentLength > 0)
             {
                 var jobSeekerUser = (JobSeekerUser)(await UserManager.FindByIdAsync(User.Identity.GetUserId()));
-                JobSeeker jobSeeker = await db.JobSeekers.FindAsync(jobSeekerUser.JobSeekerID);
+                JobSeeker jobSeeker = await bll.GetJobSeekerById(jobSeekerUser.JobSeekerID.Value);
 
                 var fileName = Path.GetFileName(file.FileName);
                 //var path = Path.Combine(Server.MapPath("~/Uploads"));
@@ -107,8 +102,10 @@ namespace Inisra_Web_App_MVC.Controllers
                 CV cv = new CV { JobSeekerID = jobSeeker.ID, Document = ms.ToArray() };
                 
                 jobSeeker.CVs.Add(cv);
-                await db.SaveChangesAsync();
- //               file.SaveAs(path);
+                using (Inisra_Web_App_MVC.DAL.InisraContext db = new DAL.InisraContext())
+                {
+                    await db.SaveChangesAsync();
+                }
             }
             return RedirectToAction("Details");
         }
@@ -146,11 +143,8 @@ namespace Inisra_Web_App_MVC.Controllers
         public async Task<ActionResult> Applications()
         {
             var jobSeekerUser = (JobSeekerUser)(await UserManager.FindByIdAsync(User.Identity.GetUserId()));
-            var applications = from a in db.Applications
-                               where a.JobSeekerID == jobSeekerUser.JobSeekerID
-                               select a;
-            applications.Include(a => a.Job).Include(a => a.Job.Company);
-            return View(await applications.ToListAsync());
+            var applications = await bll.GetApplications(jobSeekerUser.JobSeekerID.Value);
+            return View(applications);
         }
 
         //POST: Profile/DeleteApplication/5
@@ -158,16 +152,8 @@ namespace Inisra_Web_App_MVC.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> DeleteApplication (int jobID)
         {
-            Job job = await db.Jobs.FindAsync(jobID);
             var jobSeekerUser = (JobSeekerUser)(await UserManager.FindByIdAsync(User.Identity.GetUserId()));
-            try
-            {
-                var application = db.Applications.Single(a => a.JobSeekerID == jobSeekerUser.JobSeekerID && a.JobID == job.ID);
-                db.Applications.Remove(application);
-                await db.SaveChangesAsync();
-            }
-            catch (Exception) { }
-
+            await bll.DeleteApplication(jobSeekerUser.JobSeekerID.Value, jobID);
             return RedirectToAction("Applications");
         }
 
@@ -175,12 +161,8 @@ namespace Inisra_Web_App_MVC.Controllers
         public async Task<ActionResult> Invitations()
         {
             var jobSeekerUser = (JobSeekerUser)(await UserManager.FindByIdAsync(User.Identity.GetUserId()));
-
-            var invitations = from i in db.Invitations
-                              where i.JobSeekerID == jobSeekerUser.JobSeekerID
-                              select i;
-            invitations.Include(i => i.Job).Include(i => i.Job.Company);
-            return View(await invitations.ToListAsync());
+            var invitations = await bll.GetInvitaions(jobSeekerUser.JobSeekerID.Value);
+            return View(invitations);
         }
 
 
@@ -188,7 +170,7 @@ namespace Inisra_Web_App_MVC.Controllers
         {
             if (disposing)
             {
-                db.Dispose();
+                bll.Dispose();
             }
             base.Dispose(disposing);
         }
