5b596b26c8259a9729bb2bc28a94b50f80c8608e
Ferido07 <feridyz@gmail.com>
2018-05-28 22:36:44 +0300

    Solr and document objects setup.

---
+++ Inisra.Solr/Documents/JobDescription.cs
@@ -0,0 +1,13 @@
+﻿using SolrNet.Attributes;
+
+namespace Inisra.Solr.Documents
+{
+    internal class JobDescription
+    {
+        [SolrUniqueKey("id")]
+        public string Id { get; set; }
+
+        [SolrField("owner")]
+        public string Owner { get; set; }
+    }
+}
---
+++ Inisra.Solr/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+using Inisra.Solr.Documents;
+
+namespace Inisra.Solr
+{
+    public class Startup
+    {
+        private static readonly string solrCVUrl = ConfigurationManager.AppSettings["solrCVUrl"];
+        private static readonly string solrJDUrl = ConfigurationManager.AppSettings["solrJDUrl"];
+
+        public static void Init()
+        {    
+            SolrNet.Startup.Init<CV>(solrCVUrl);
+            SolrNet.Startup.Init<JobDescription>(solrJDUrl);
+        }
+    }
+}
