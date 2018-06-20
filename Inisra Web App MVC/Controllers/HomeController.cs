e51f101fc287f879bfa7237cfba9ff1acba2ae21
Ferido07 <feridyz@gmail.com>
2018-06-20 06:02:02 +0300

    Implemented and Tested Document Searching

--- Inisra Web App MVC/Controllers/HomeController.cs
+++ Inisra Web App MVC/Controllers/HomeController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Threading.Tasks;
 
 namespace Inisra_Web_App_MVC.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index(string query,string documentType)
         {
-            return View();
+            if (String.IsNullOrEmpty(query))
+                return View();
+            else
+            {
+                if (String.Equals(documentType, "CV")) {
+                    var cvDocuments = await Inisra.Solr.Operations.SolrOperations.CVQueryAsync(query);
+                    return View("CVsResult", cvDocuments);
+                }
+                else { 
+                    var jdDocuments = await Inisra.Solr.Operations.SolrOperations.JobDescriptionQueryAsync(query);
+                    return View("JDsResult", jdDocuments);
+                }
+            }
         }
 
         public ActionResult About()
